406b2d3e239d9e918ae6b6e11121b3ce3564e805
bombifighter <botond20@gmail.com>
2023-04-25 22:56:14 +0200

    Fix tests

--- Measurements/measurement_1.py
+++ Measurements/measurement_1.py
@@ -4,24 +4,29 @@ import time
 if __name__ == '__main__':
 
     headers = {
-    'Content-Type': 'text/plain'
+    'Content-Type': 'application/json'
     }
+    
+    timeSum = 0
 
-    start = time.time()
-    for i in range(1, 1000):
+    for i in range(5):
+    
+        start = time.time()
         
-        url = "http://127.0.0.1:3000/guest/insertGuest"
-        payload = "{\"id\":1 , \"name\": \"Abel\", \"gender\":\"male\", \"bdate\": \"2002.03.11\", \"phone\":\"+36301112233\", \"email\":\"abel@mail.com\"}"
-        response = requests.request("POST", url, headers=headers, data=payload)
-        
-        url = "http://127.0.0.1:3000/guest/updateGuest/1"
-        payload = "{\"id\":1 , \"name\": \"Doma\", \"gender\":\"male\", \"bdate\": \"2002.03.11\", \"phone\":\"+36301112233\", \"email\":\"abel@mail.com\"}"
-        response = requests.request("PUT", url, headers=headers, data=payload)
-        
-        url = "http://127.0.0.1:3000/guest/deleteGuest/1"
-        response = requests.request("DELETE", url, headers=headers, data=payload)
+        for i in range(1, 10):
+            
+            url = "http://127.0.0.1:3000/guest/insertGuest"
+            payload = "{\"id\":1 , \"name\": \"Abel\", \"gender\":\"male\", \"bdate\": \"2002.03.11\", \"phone\":\"+36301112233\", \"email\":\"abel@mail.com\"}"
+            response = requests.request("POST", url, headers=headers, data=payload)
+            
+            url = "http://127.0.0.1:3000/guest/updateGuest/1"
+            payload = "{\"id\":1 , \"name\": \"Doma\", \"gender\":\"male\", \"bdate\": \"2002.03.11\", \"phone\":\"+36301112233\", \"email\":\"abel@mail.com\"}"
+            response = requests.request("PUT", url, headers=headers, data=payload)
+            
+            url = "http://127.0.0.1:3000/guest/deleteGuest/1"
+            response = requests.request("DELETE", url, headers=headers, data=payload)
 
-    end = time.time()
-    time = end-start
+        end = time.time()
+        timeSum = timeSum + (end - start)
 
-    print(time)
+    print(timeSum/5)
--- Measurements/measurement_2.py
+++ Measurements/measurement_2.py
@@ -4,9 +4,11 @@ import time
 if __name__ == '__main__':
 
     headers = {
-    'Content-Type': 'text/plain'
+    'Content-Type': 'application/json'
     }
-
+    
+    timeSum = 0
+    
     url = "http://127.0.0.1:3000/guest/insertGuest"
     payload = "{\"id\":1 , \"name\": \"Abel\", \"gender\":\"male\", \"bdate\": \"2002.03.11\", \"phone\":\"+36301112233\", \"email\":\"abel@mail.com\"}"
     response = requests.request("POST", url, headers=headers, data=payload)
@@ -19,24 +21,24 @@ if __name__ == '__main__':
     payload = "{\"id\": 1,\"guestId\": 1, \"membershipTypeId\": 1,\"endDate\": \"2023/10/01\",\"occasionsLeft\": -1 }"
     response = requests.request("POST", url, headers=headers, data=payload)
 
-    start = time.time()
-##########################################
+    for i in range(5):
 
-    for i in range(1000):
-        url = "http://127.0.0.1:3000/guest/checkin"
-        payload = "{\"guestId\": 1, \"gender\": \"male\", \"date\": \"2023/04/21\", \"time\": \"00:40\" }"
-        response = requests.request("POST", url, headers=headers, data=payload)
+        start = time.time()
 
-        url = "http://127.0.0.1:3000/guest/checkout"
-        payload = "{\"guestId\": 1, \"lockerId\": 1, \"gender\": \"male\", \"dateTime\": \"2023/04/21 : 00:41\" }"
-        response = requests.request("POST", url, headers=headers, data=payload)
+        for i in range(1,10):
+            url = "http://127.0.0.1:3000/guest/checkin"
+            payload = "{\"guestId\": 1, \"gender\": \"male\", \"date\": \"2023/04/21\", \"time\": \"00:40\" }"
+            response = requests.request("POST", url, headers=headers, data=payload)
 
+            url = "http://127.0.0.1:3000/guest/checkout"
+            payload = "{\"guestId\": 1, \"lockerId\": 1, \"gender\": \"male\", \"dateTime\": \"2023/04/21 : 00:41\" }"
+            response = requests.request("POST", url, headers=headers, data=payload)
 
-##########################################
-    end = time.time()
-    time = end - start
+        end = time.time()
+        timeSum = timeSum + (end - start)
 
+    
     url = "http://127.0.0.1:3000/guest/deleteGuest/1"
     response = requests.request("DELETE", url, headers=headers)
 
-    print(time)
+    print(timeSum/5)
